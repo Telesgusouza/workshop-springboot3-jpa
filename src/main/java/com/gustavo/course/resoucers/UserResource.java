8f9cb86623297309feb9a79d0a12760e895a547e
Gustavo Teles <gustavo.teles711@gmail.com>
2024-01-14 19:50:42 -0300

    User Insert

--- src/main/java/com/gustavo/course/resoucers/UserResource.java
+++ src/main/java/com/gustavo/course/resoucers/UserResource.java
@@ -1,39 +1,47 @@
 package com.gustavo.course.resoucers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gustavo.course.entities.User;
 import com.gustavo.course.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		
+
 		List<User> list = service.findAll();
-		
+
 		return ResponseEntity.ok().body(list);
 	}
-	
-	// já que é nescessario passar um id então passamos por aqui também
-	// também precisamos passar um novo @ para a variavel, isso para dizer que o id do 
-	// prametro também vai na url
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// gerar a uri
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
