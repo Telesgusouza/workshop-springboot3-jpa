ea371b1b0158d969425e98567bcc58550c0863e3
Gustavo Teles <gustavo.teles711@gmail.com>
2024-01-14 20:04:36 -0300

    User delete

--- src/main/java/com/gustavo/course/resoucers/UserResource.java
+++ src/main/java/com/gustavo/course/resoucers/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,8 +41,25 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
-		// gerar a uri
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	/*
+	
+	como uso o id precisamos passar para ele o id
+	 
+	coloquei como void por que ele n me retorna nd
+	
+	para o id ser reconhecido como variavel da url usamos o 
+	@PathVariable
+	
+	*/
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
